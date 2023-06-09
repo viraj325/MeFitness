c38263a9a7067ec01971d0b4cb0fc952e632370f
Viraj Patel <virajpatel325@gmail.com>
2023-06-09 17:53:07 -0400

    update

--- lib/localshared.dart
+++ lib/localshared.dart
@@ -1,6 +1,23 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalShared {
+  static Future<String> read() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    // Try reading data from the 'counter' key. If it doesn't exist, returns null.
+    final int? counter = prefs.getInt('counter');
+    // Try reading data from the 'repeat' key. If it doesn't exist, returns null.
+    final bool? repeat = prefs.getBool('repeat');
+    // Try reading data from the 'decimal' key. If it doesn't exist, returns null.
+    final double? decimal = prefs.getDouble('decimal');
+    // Try reading data from the 'action' key. If it doesn't exist, returns null.
+    final String? action = prefs.getString('action');
+    // Try reading data from the 'items' key. If it doesn't exist, returns null.
+    final List<String>? items = prefs.getStringList('items');
+
+    return "Some string";
+  }
+
   static Future<void> write() async {
     // Obtain shared preferences.
     final SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -17,20 +34,20 @@ class LocalShared {
     await prefs.setStringList('items', <String>['Earth', 'Moon', 'Sun']);
   }
 
-  static Future<String> read() async {
+  static Future<void> update() async {
+    // Obtain shared preferences.
     final SharedPreferences prefs = await SharedPreferences.getInstance();
+  }
 
-    // Try reading data from the 'counter' key. If it doesn't exist, returns null.
-    final int? counter = prefs.getInt('counter');
-    // Try reading data from the 'repeat' key. If it doesn't exist, returns null.
-    final bool? repeat = prefs.getBool('repeat');
-    // Try reading data from the 'decimal' key. If it doesn't exist, returns null.
-    final double? decimal = prefs.getDouble('decimal');
-    // Try reading data from the 'action' key. If it doesn't exist, returns null.
-    final String? action = prefs.getString('action');
-    // Try reading data from the 'items' key. If it doesn't exist, returns null.
-    final List<String>? items = prefs.getStringList('items');
+  static Future<void> delete(String key) async {
+    // Obtain shared preferences.
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove(key);
+  }
 
-    return "Some string";
+  static Future<void> deleteAll() async {
+    // Obtain shared preferences.
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
   }
 }
